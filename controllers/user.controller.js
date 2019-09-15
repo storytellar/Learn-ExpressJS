64cc88084f0776e1b0116c5561e8b75a850ada48
storytellar <storytellar12@gmail.com>
2019-09-15 15:00:41 +0700

    New layouts and UI/UX improvement

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 const db = require('../db');
+const shortid = require('shortid');
+
 
 module.exports.index = function (req, res) {
     res.render('users/index', {
@@ -9,7 +11,7 @@ module.exports.index = function (req, res) {
 module.exports.search = function (req, res) {
     var q = req.query.q;
     var matchedUsers = db.get('users').value().filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+        return change_alias(user.name).toLowerCase().indexOf(change_alias(q).toLowerCase()) !== -1;
     });
 
     res.render('users/index', {
@@ -37,3 +39,23 @@ module.exports.get = function (req, res) {
         user: user
     });
 };
+
+
+
+
+
+function change_alias(alias) {
+    var str = alias;
+    str = str.toLowerCase();
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o"); 
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
+    str = str.replace(/đ/g,"d");
+    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+    str = str.replace(/ + /g," ");
+    str = str.trim(); 
+    return str;
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var userRoute = require('./routes/user.route')
+const userRoute = require('./routes/user.route')
+
+const app = express();
 
 const port = 3000;
-const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -13,14 +14,10 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/users', userRoute);
 
-
 // render index
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-
-app.listen(port, ()=>{
-    console.log('===> OPEN: http://localhost:' + port);
-});
+app.listen(port, ()=>console.log('===> OPEN: http://localhost:' + port));
 
