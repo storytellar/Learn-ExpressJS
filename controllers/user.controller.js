e138bfba11d1a64f7fd284bb2b1842e09b5c290c
storytellar <storytellar12@gmail.com>
2019-09-15 16:34:49 +0700

    Middleware user validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,26 +20,6 @@ module.exports.search = function (req, res) {
 };
 
 module.exports.postCreate = function (req, res) {
-    // validate
-    var errors = [];
-    if (!req.body.name)
-    {
-        errors.push('Name is required.');
-    }
-
-    if (!req.body.phone)
-    {
-        errors.push('Phone is required.');
-    }
-
-    if (errors.length) {
-        res.render('users/create', {
-            errors: errors,
-            values: req.body
-        });
-        return;
-    }
-
     req.body.id = shortid.generate();
     db.get('users').push(req.body).write();
     res.redirect('/users');
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller');
+var validate = require('../validates/user.validate');
 
 var router = express.Router();
 
@@ -10,7 +11,7 @@ router.get('/search', controller.search);
 
 router.get('/create', controller.create);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate ,controller.postCreate);
 
 router.get('/:id', controller.get);
 
